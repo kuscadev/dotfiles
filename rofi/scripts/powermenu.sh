ea9ecfb9bba045b5efebb0b6a8bb6abea583439c
Oğuzhan Kuşca <oguzhankusca@outlook.com>
2025-05-29 17:23:03 +0300

    bspwm/sxhkd config eklendi.

---
+++ rofi/scripts/powermenu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+options="󰍃 Logout\n󰐥 Shutdown\n󰑐 Reboot"
+
+chosen=$(echo -e "$options" | rofi -dmenu -theme ~/.config/rofi/themes/powermenu.rasi -p "Power Menu" -i)
+
+case "$chosen" in
+    "󰍃 Logout") bspc quit ;;
+    "󰐥 Shutdown") poweroff ;;
+    "󰑐 Reboot") reboot;;
+esac
